56eeadfb05bc09c19193335ce0ae7a7a2f930b17
Stefan Werfling <stefan.werfling@googlemail.com>
2025-09-23 14:42:05 +0200

    fix json limit (Internal server error: request entity too large)

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,9 @@ function expressMiddleware(): Plugin {
         name: 'vite-express-middleware',
         configureServer(server) {
             const app = express();
-            app.use(express.json());
+
+            app.use(express.json({ limit: "50mb" }));
+            app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
             // ---------------------------------------------------------------------------------------------------------
 
